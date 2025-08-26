d2c1cd10fbd1fe730578c015565d26c623ca9b0b
jon2502 <jonbjarke25@outlook.dk>
2025-08-26 13:25:59 +0200

    setup of rputing and some components

---
+++ components/Navbar.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Navbar() {
+  return (
+  <header className='px-8 pt-3 pb-16'>
+      <nav className="flex justify-between items-center">
+          <div className='flex items-center gap-5'>
+            <Link href="/Decks">
+                <span>Decks</span>
+            </Link>
+            <Link href="/Create">
+                <span>Create Deck</span>
+            </Link>
+          </div>
+      </nav>
+  </header>
+  )
+}
+
+export default Navbar
