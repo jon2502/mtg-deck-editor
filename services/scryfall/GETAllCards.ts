6198a8ff2ecc6fd57202c42bbe66ed2f1cc264e9
jon2502 <jonbjarke25@outlook.dk>
2025-08-26 17:12:26 +0200

    setup of seach function

---
+++ services/scryfall/GETAllCards.ts
@@ -0,0 +1,35 @@
+
+interface SearchParams {
+  name?: string;
+  format?: string; // e.g. 'standard', 'modern'
+  color?: string;
+  page?: number;
+  // Extend with filters like color, set, etc. if needed
+}
+
+export const searchCards = async (params: SearchParams) => {
+    const { name, format, color, page} = params;
+
+    const searchIndex = [
+        {value: name ? `name:${name}` : '' },
+        {value: format ? `f:${format}` : '' },
+        {value: color && format == "commander" ? `commander:${color}` : color ? `color:${color}` : ''}
+    ]
+
+    const queryString = searchIndex
+        .map(filter => filter.value)
+        .filter(Boolean) // Remove empty strings
+        .join(' ');
+    
+    //insert filterd content into url and use encodeURIComponent to encode character of the url
+    var url = `https://api.scryfall.com/cards/search?q=${encodeURIComponent(queryString)}${page ? `&page=${page}` : ''}`;
+    
+    const res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Scryfall search failed: ${res.status}`);
+    }
+    
+    return res.json();
+
+}
