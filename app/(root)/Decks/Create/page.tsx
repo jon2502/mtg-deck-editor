14f7f71c9a885b688f42f5c6f6196c00363b0256
jon2502 <jonbjarke25@outlook.dk>
2025-09-05 13:02:05 +0200

    setup deck creation page

--- app/(root)/Decks/Create/page.tsx
+++ app/(root)/Decks/Create/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
 
 const page = () => {
+  const [format, setFormat] = useState("")
+  const [name, setName]= useState("")
+
+
+  async function CreateDeck(){
+    fetch("",{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            name: name,
+            format: format
+          })
+    })
+  }
+
   return (
-    <div>page</div>
+    <div>
+
+      <button onClick={() => CreateDeck()}></button>
+    </div>
   )
 }
 
