2c9dd8e60f2ffd83a2dde30952623fff5bbcdb2b
jon2502 <113421076+jon2502@users.noreply.github.com>
2026-08-20 18:08:35 +0200

    fixed up config issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,7 @@ import Overlay from "@/components/Overlay";
 import {Overlaysetting} from "@/context/overlay_context"
 import {Decksetting} from "@/context/deck_context"
 
-// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
-// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
-export const instant = false;
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
